c5999a7372ef3c808225e5321a66ac337b13daa9
Axel <axelcorjmaxd@gmail.com>
2023-08-11 11:22:17 -0600

    vercel

--- js/app.js
+++ js/app.js
@@ -16,26 +16,3 @@ formulario.addEventListener("submit", (e) => {
 });
  
 // app.js
-
-
-  // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBOQBXqSQK46mv8OHM2gxdgNhvJOMfXq8Q",
-  authDomain: "alurageek-1589b.firebaseapp.com",
-  projectId: "alurageek-1589b",
-  storageBucket: "alurageek-1589b.appspot.com",
-  messagingSenderId: "896721029931",
-  appId: "1:896721029931:web:0e59aa500e7d1659d0936e",
-  measurementId: "G-3N7JYL5GPQ"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
